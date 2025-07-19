482c82bf6ffcae542ce6a26e384f00a997f8597d
Julian Hikaru Payne <j-payne@free-will.co>
2025-07-19 20:47:18 +0900

    done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 "use strict"
 
-const contactForm = document.getElementById('contact-form');
+const contactForm = document.querySelector('.contact-form');
+const email = document.querySelector('.contact-form__email');
+const content = document.querySelector('.contact-form__content');
 
 contactForm.addEventListener('submit', $event => {
-  alert($event.returnValue);
+  alert('submitted value: ' + JSON.stringify({ email: email.value, content: content.value }, null, 2));
 })
